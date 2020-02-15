78abde2454f30461e9faa25ce5cd62db43ce57a7
Jean Raphaël Bordet <jr.bordet@synesthesia.it>
2020-02-15 20:14:19 +0100

    Update playgorund

--- PrimeTime.playground/Contents.swift
+++ PrimeTime.playground/Contents.swift
@@ -12,37 +12,46 @@ enum PrimeTimeScene {
 
 let currentScene = PrimeTimeScene.counter
 
+var currentViewController = UIViewController.init()
+
 switch currentScene {
 case .counter:
     let counterScene = Scene<CounterViewController>().render()
 
-    counterScene.store = Store<CounterViewState, CounterViewAction>(initialValue: CounterViewState(count: 0,
-                                                                                                   favoritePrimes: [2, 5, 7],
-                                                                                                   isLoading: false,
-                                                                                                   alertNthPrime: nil),
-                                                                    reducer: counterViewReducer)
+    let state = CounterViewState(
+        count: 0,
+        favoritePrimes: [2, 5, 7],
+        isLoading: false,
+        alertNthPrime: nil
+    )
+    
+    counterScene.store = Store<CounterViewState, CounterViewAction>(initialValue: state, reducer: counterViewReducer)
     
     let nvc = UINavigationController(rootViewController: counterScene)
     
-    PlaygroundPage.current.liveView = nvc
+    currentViewController = nvc
 
     break
 case .primemodal:
     let primeModalScene = Scene<PrimeModalViewController>().render()
-
-    primeModalScene.store = Store<PrimeModalState, PrimeModalAction>(initialValue: (count: 2, favoritePrimes: [2, 5, 7]), reducer: primeModalReducer(state:action:))
     
-    PlaygroundPage.current.liveView = primeModalScene
+    let state = (count: 2, favoritePrimes: [2, 5, 7])
+
+    primeModalScene.store = Store<PrimeModalState, PrimeModalAction>(initialValue: state, reducer: primeModalReducer(state:action:))
+    
+    currentViewController = primeModalScene
 
     break
 case .favoritePrimes:
     let favoritePrimesScene = Scene<FavoritePrimesViewController>().render()
 
-    favoritePrimesScene.store = Store(initialValue: [2, 3, 5], reducer: favoritePrimesReducer(state:action:))
+    let state = [2, 3, 5]
     
-    PlaygroundPage.current.liveView = favoritePrimesScene
+    favoritePrimesScene.store = Store(initialValue: state, reducer: favoritePrimesReducer(state:action:))
+    
+    currentViewController = favoritePrimesScene
 
     break
 }
 
-// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = currentViewController
