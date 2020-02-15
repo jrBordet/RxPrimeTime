8eeb236954233546bf56d8bd80c87ca6a47e1290
Jean Raphaël Bordet <jr.bordet@synesthesia.it>
2020-02-15 20:06:11 +0100

    Update playgorund

--- PrimeTime.playground/Contents.swift
+++ PrimeTime.playground/Contents.swift
@@ -6,21 +6,43 @@ import Counter
 import ComposableArchitecture
 import FavoritePrimes
 
-let primeModalScene = Scene<PrimeModalViewController>().render()
+enum PrimeTimeScene {
+    case counter, primemodal, favoritePrimes
+}
 
-primeModalScene.store = Store<PrimeModalState, PrimeModalAction>(initialValue: (count: 2, favoritePrimes: [2, 5, 7]), reducer: primeModalReducer(state:action:))
+let currentScene = PrimeTimeScene.counter
 
-let counterScene = Scene<CounterViewController>().render()
+switch currentScene {
+case .counter:
+    let counterScene = Scene<CounterViewController>().render()
 
-counterScene.store = Store<CounterViewState, CounterViewAction>(initialValue: CounterViewState(count: 0,
-                                                                                               favoritePrimes: [2, 5, 7],
-                                                                                               isLoading: false,
-                                                                                               alertNthPrime: nil),
-                                                                reducer: counterViewReducer)
+    counterScene.store = Store<CounterViewState, CounterViewAction>(initialValue: CounterViewState(count: 0,
+                                                                                                   favoritePrimes: [2, 5, 7],
+                                                                                                   isLoading: false,
+                                                                                                   alertNthPrime: nil),
+                                                                    reducer: counterViewReducer)
+    
+    let nvc = UINavigationController(rootViewController: counterScene)
+    
+    PlaygroundPage.current.liveView = nvc
 
-let favoritePrimesScene = Scene<FavoritePrimesViewController>().render()
+    break
+case .primemodal:
+    let primeModalScene = Scene<PrimeModalViewController>().render()
 
-favoritePrimesScene.store = Store(initialValue: [2, 3, 5], reducer: favoritePrimesReducer(state:action:))
+    primeModalScene.store = Store<PrimeModalState, PrimeModalAction>(initialValue: (count: 2, favoritePrimes: [2, 5, 7]), reducer: primeModalReducer(state:action:))
+    
+    PlaygroundPage.current.liveView = primeModalScene
+
+    break
+case .favoritePrimes:
+    let favoritePrimesScene = Scene<FavoritePrimesViewController>().render()
+
+    favoritePrimesScene.store = Store(initialValue: [2, 3, 5], reducer: favoritePrimesReducer(state:action:))
+    
+    PlaygroundPage.current.liveView = favoritePrimesScene
+
+    break
+}
 
 // Present the view controller in the Live View window
-PlaygroundPage.current.liveView = favoritePrimesScene
