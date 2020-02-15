99f0461f383eacfaa5b46ffe2696011cd2d11f3d
Jean Raphaël Bordet <jr.bordet@synesthesia.it>
2020-02-15 09:15:07 +0100

    Wip on Counter

---
+++ PrimeTime.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+import Counter
+
+ let rootScene = Scene<PrimeModalViewController>().render()
+
+
+
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = rootScene
+    //MyViewController()
---
+++ PrimeTime.playground/Sources/SceneBuilder.swift
@@ -0,0 +1,32 @@
+import Foundation
+import UIKit
+
+public struct Scene<A: UIViewController> {
+    public init () { }
+    
+    public func render() -> A {
+        guard let nib = (String(describing: type(of: self)) as NSString).components(separatedBy: ".").first else {
+            fatalError()
+        }
+                
+        let vc = A(nibName: String(nib.replacingOccurrences(of: "Scene<", with: "").dropLast()),
+                   bundle: Bundle(for: A.self))
+        
+        return vc
+    }
+}
+
+public func navigationLink<A: UIViewController>(from vc: UIViewController, destination: Scene<A>, completion: (A) -> Void, isModal: Bool =  false) -> Void {
+    let to = destination.render()
+    
+    if isModal {
+        completion(to)
+        
+        vc.present(to, animated: true)
+        return
+    }
+    
+    vc.navigationController?.pushViewController(to, animated: true)
+    
+    completion(to)
+}
